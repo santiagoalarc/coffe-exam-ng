619c8598f88e1e289c6cda4f0f2a2c7c32b340a2
santiago.alarcon <santiago.alarcon@sofka.com.co>
2023-11-04 02:09:03 -0500

    Se agregan test unitarios

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CoffeeListComponent } from './coffee/coffee-list/coffee-list.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+ beforeEach(async () => {
+   await TestBed.configureTestingModule({
+     imports: [RouterTestingModule, HttpClientModule],
+     declarations: [AppComponent, CoffeeListComponent],
+   }).compileComponents();
+ });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+ it('should create the app', () => {
+   const fixture = TestBed.createComponent(AppComponent);
+   const app = fixture.componentInstance;
+   expect(app).toBeTruthy();
+ });
 
-  it(`should have as title 'coffeapp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('coffeapp');
-  });
+ it(`should have as title 'base-project'`, () => {
+   const fixture = TestBed.createComponent(AppComponent);
+   const app = fixture.componentInstance;
+   expect(app.title).toEqual('coffeapp');
+ });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('coffeapp app is running!');
-  });
 });
--- src/app/coffee/coffee.service.spec.ts
+++ src/app/coffee/coffee.service.spec.ts
@@ -2,10 +2,12 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { CoffeeService } from './coffee.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: Coffee', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [CoffeeService]
     });
   });

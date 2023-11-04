d11658ec0f055c0306ad2e1eead1201d838a967f
santiago.alarcon <santiago.alarcon@sofka.com.co>
2023-11-04 01:14:39 -0500

    Se crean estilos de la pagina

--- src/app/coffee/coffee-list/coffee-list.component.ts
+++ src/app/coffee/coffee-list/coffee-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Coffee } from '../coffee';
+import { CoffeeService } from '../coffee.service';
 
 @Component({
   selector: 'app-coffee-list',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoffeeListComponent implements OnInit {
 
-  constructor() { }
+  coffees: Array<Coffee> = [];
+  totalBlend!: number;
+  totalOrigen!: number;
+
+  constructor(private coffeeService: CoffeeService) { }
+
+  getCoffees(): void{
+    this.coffeeService.getCoffees().subscribe((coffees) => {
+      this.coffees = coffees;
+      this.totalBlend = coffees.filter(coffe => coffe.tipo === "Café de Origen").length;
+      this.totalOrigen = coffees.filter(coffe => coffe.tipo === "Blend").length;
+    })
+  }
 
   ngOnInit() {
+    this.getCoffees();
   }
 
 }

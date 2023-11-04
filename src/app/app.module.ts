e4f6202be6ac14240935eafd4f0a8b6e9d92701a
santiago.alarcon <santiago.alarcon@sofka.com.co>
2023-11-03 23:09:02 -0500

    Se configura el app para recibir la importación del módulo de coffee y pueda ser deplagado el componente en este

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoffeeModule } from './coffee/coffee.module';
 
 
 @NgModule({
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoffeeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/coffee/coffee.module.ts
+++ src/app/coffee/coffee.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  exports: [CoffeeListComponent],
+  declarations: [CoffeeListComponent]
 })
 export class CoffeeModule { }
